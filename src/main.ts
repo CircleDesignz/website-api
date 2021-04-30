5edd850225d3c6b6628902326cbfdd581d9da33d
Felix <felix.zhou@mail.utoronto.ca>
2021-04-30 01:00:34 -0400

    Remove microservices

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.connectMicroservice({
-    transport: Transport.TCP,
-  })
 
-  await app.startAllMicroservicesAsync();
   await app.listen(3000);
 }
 bootstrap();
+
