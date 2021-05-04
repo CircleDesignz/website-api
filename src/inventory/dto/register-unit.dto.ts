6aa06d4559cef4e2530a54fa328f9e030d766730
Felix <felix.zhou@mail.utoronto.ca>
2021-05-04 04:54:19 -0400

    add validators

--- src/inventory/dto/register-unit.dto.ts
+++ src/inventory/dto/register-unit.dto.ts
@@ -1,8 +1,25 @@
+import { IsString, IsNumber, IsInt, Min } from 'class-validator';
+
 export class RegisterUnitDto {
+  @IsString()
   sku: string;
+
+  @IsString()
   name: string;
+
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @Min(0)
   costInCad: number;
+
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @Min(0)
   priceInCad: number;
+
+  @IsInt()
+  @Min(0)
   stock: number;
+
+  @IsNumber()
+  @Min(0)
   weightInKg: number;
 }
