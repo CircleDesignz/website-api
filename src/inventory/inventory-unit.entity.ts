74e4045de0f39175e34d02329e439859709b0245
Felix <felix.zhou@mail.utoronto.ca>
2021-05-03 21:35:50 -0400

    postgres money -> numeric

--- src/inventory/inventory-unit.entity.ts
+++ src/inventory/inventory-unit.entity.ts
@@ -2,28 +2,23 @@ import { Entity, Column, PrimaryColumn } from 'typeorm';
 
 @Entity()
 export class InventoryUnit {
-  public constructor(init?:Partial<InventoryUnit>) {
-    Object.assign(this, init);
-  }
-
   @PrimaryColumn()
   sku: string;
 
   @Column()
   name: string;
 
-  // TODO: Maybe convert to "numeric" type, see https://wiki.postgresql.org/wiki/Don%27t_Do_This#Don.27t_use_money
-  @Column({ type: "money" })
-  unitCost: number;
+  @Column({ type: "numeric" })
+  costInCad: number;
 
-  @Column({ type: "money" })
-  unitPrice: number;
+  @Column({ type: "numeric" })
+  priceInCad: number;
 
   @Column({ type: "integer" })
   currentStock: number;
 
   @Column({ type: "double precision" })
-  weight: number;
+  weightInKg: number;
 
   @Column({ type: "boolean" })
   isArchived: boolean;
