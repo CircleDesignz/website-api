9485876e12d1acd5016b90b85c81a1f63a348c65
Felix <felix.zhou@mail.utoronto.ca>
2021-05-25 18:19:01 -0400

    clean up

--- src/controllers/stockroom/stockroom.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StockroomController } from '@controllers/stockroom/stockroom.controller';
-import { ServicesModule } from '@services/services.module';
-
-@Module({
-  controllers: [StockroomController],
-  imports: [ServicesModule]
-})
-export class StockroomModule {}
