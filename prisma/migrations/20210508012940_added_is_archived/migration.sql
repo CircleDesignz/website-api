cfa67e50bee8076ba1cc6149593f37db5827245f
Felix <felix.zhou@mail.utoronto.ca>
2021-05-25 18:18:08 -0400

    prisma to typeorm

--- prisma/migrations/20210508012940_added_is_archived/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "StockEntity" ADD COLUMN     "isArchived" BOOLEAN NOT NULL DEFAULT false;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
-import { StockroomModule } from '@controllers/stockroom/stockroom.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StockroomModule } from '@controllers/inventory/inventory.module';
 import { ServicesModule } from '@services/services.module';
+import { InventoryUnit } from '@common/entities/inventory/inventory-unit.entity';
+import { Product } from '@common/entities/inventory/product.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'circle_inventory',
+      username: 'fx',
+      password: '162301',
+      entities: [InventoryUnit, Product],
+      synchronize: true, // TODO: Set false for production
+    }),
     StockroomModule,
     ServicesModule,
   ],
