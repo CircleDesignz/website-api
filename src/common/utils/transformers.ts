420ecc470758b9848585a836a46687a09b5411da
Felix <felix.zhou@mail.utoronto.ca>
2021-06-01 18:25:01 -0400

    fix transformer

--- src/common/utils/transformers.ts
+++ src/common/utils/transformers.ts
@@ -2,8 +2,12 @@ import { ValueTransformer } from 'typeorm';
 import { DEFAULT_CURRENCY } from '@common/constants/currency';
 import Dinero from 'dinero.js';
 
+// TODO: Probably a better way to do this
 export const currencyTransformer: ValueTransformer = {
-  to: (value: Dinero.Dinero) => value.getAmount(),
+  to: (value: Dinero.Dinero | undefined) =>
+    value === undefined ? null : value.getAmount(),
   from: (value: number) =>
-    Dinero({ amount: value, currency: DEFAULT_CURRENCY }),
+    value === null
+      ? null
+      : Dinero({ amount: value, currency: DEFAULT_CURRENCY }),
 };
