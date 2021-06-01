062a17d0314da70906c91f37b1e51ecf6c2f9b25
Felix <felix.zhou@mail.utoronto.ca>
2021-06-01 18:19:19 -0400

    move tests to testing mod

--- test/inventory/inventory.controller.spec.ts
+++ test/inventory/inventory.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { InventoryController } from './inventory.controller';
+import { InventoryController } from '../../src/core/inventory/inventory.controller';
 
 describe('InventoryController', () => {
   let controller: InventoryController;
