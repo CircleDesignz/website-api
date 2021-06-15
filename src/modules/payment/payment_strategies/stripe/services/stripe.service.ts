f43a6f0cc88fa6997e9d57b48d00ab78c86abe16
Felix <felix.zhou@mail.utoronto.ca>
2021-06-15 16:40:53 -0400

    clean up stripe submodule DI tokens

--- src/modules/payment/payment_strategies/stripe/services/stripe.service.ts
+++ src/modules/payment/payment_strategies/stripe/services/stripe.service.ts
@@ -1,10 +1,11 @@
 import { Inject, Injectable } from '@nestjs/common';
 import stripe from 'stripe';
+import { STRIPE_INJECTION_TOKEN } from '../constants/di-tokens';
 
 @Injectable()
 export class StripeService {
   constructor(
-    @Inject('STRIPE_INJECTION_TOKEN') private readonly _stripeInstance: stripe
+    @Inject(STRIPE_INJECTION_TOKEN) private readonly _stripeInstance: stripe
   ) {}
 
   async test(): Promise<any> {
--- src/modules/payment/payment_strategies/stripe/stripe.module.ts
+++ src/modules/payment/payment_strategies/stripe/stripe.module.ts
@@ -3,14 +3,13 @@ import { StripeController } from './controllers/stripe.controller';
 import { StripeService } from './services/stripe.service';
 import stripe from 'stripe';
 import { ConfigService } from '@nestjs/config';
-
-const stripe_injection_token = "STRIPE_INJECTION_TOKEN" // TODO: move to constants
+import { STRIPE_INJECTION_TOKEN } from './constants/di-tokens';
 
 @Module({
   providers: [
     StripeService,
     {
-      provide: stripe_injection_token,
+      provide: STRIPE_INJECTION_TOKEN,
       useFactory: async (configService: ConfigService) => {
         return new stripe.Stripe(configService.get('STRIPE_TOKEN'), {
           apiVersion: '2020-08-27',
