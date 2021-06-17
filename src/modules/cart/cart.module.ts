d411b3b8be1a43c53cdac858c745ea10c73bbeb6
Felix <felix.zhou@mail.utoronto.ca>
2021-06-17 02:35:49 -0400

    clean up

--- src/modules/cart/cart.module.ts
+++ src/modules/cart/cart.module.ts
@@ -4,13 +4,14 @@ import { CartController } from './controllers/cart.controller';
 import { CartService } from './services/cart.service';
 import redis from 'redis';
 import { CartRepository } from './repositories/cart.repository';
+import { REDIS_TOKEN } from './constants/di-tokens';
 
 @Module({
   providers: [
     CartService,
     CartRepository,
     {
-      provide: 'redis_di',
+      provide: REDIS_TOKEN,
       useFactory: async (configService: ConfigService) => {
         const client = redis.createClient({
           host: configService.get('REDIS_HOST'),
--- src/modules/cart/repositories/cart.repository.ts
+++ src/modules/cart/repositories/cart.repository.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { RedisClient } from 'redis';
 import { promisify } from 'util';
+import { REDIS_TOKEN } from '../constants/di-tokens';
 
 @Injectable()
 export class CartRepository {
@@ -8,17 +9,19 @@ export class CartRepository {
   private _async_hset: any;
   private _async_del: any;
 
-  constructor(@Inject('redis_di') private readonly _redisClient: RedisClient) {
+  constructor(@Inject(REDIS_TOKEN) private readonly _redisClient: RedisClient) {
     this._async_hgetall = promisify(this._redisClient.hgetall).bind(
       this._redisClient
     );
-    this._async_hset = promisify(this._redisClient.hset).bind(this._redisClient);
+    this._async_hset = promisify(this._redisClient.hset).bind(
+      this._redisClient
+    );
     this._async_del = promisify(this._redisClient.del).bind(this._redisClient);
   }
 
   async findMany(key: string): Promise<{ [key: string]: string }> {
     return this._async_hgetall(key);
-  };
+  }
 
   async setHash(key: string, args: string[]): Promise<boolean> {
     return this._async_hset(key, args);
